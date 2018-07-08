4fe6c024c7d427d03af66581b1d9efb642d7d0e5
carriexu24 <cxu24@usfca.edu>
2018-07-07 22:16:34 -0700

    pep8

--- fix_search.py
+++ fix_search.py
@@ -1,5 +1,5 @@
 class WordDic:
-    """A reusable dictionary of all matching words in a given list of words 
+    """A reusable dictionary of all matching words in a given list of words
        for a specific prefix or suffix.
 
     Attributes:
@@ -24,7 +24,7 @@ class WordDic:
             for x in range(1, len(word) + 1):
                 prefix = word[:x]
                 suffix = word[x-1:]
-                
+
                 try:
                     self.p_map[prefix].append(idx)
                 except KeyError:
@@ -47,7 +47,7 @@ class WordDic:
             return None
         else:
             return [self.words[i] for i in idxs]
-    
+
     def get_s_words(self, suffix):
         """
         For a given suffix, return a list of all matching words.
@@ -66,12 +66,14 @@ def control(args, words):
     """Control for Constraints.
     Only one option at the time should be supported.
     Prefix or suffix must contain at least one character.
-    Prefix or suffix only include letters from English ASCII character set 
+    Prefix or suffix only include letters from English ASCII character set
     in the range from DEC 65 to DEC 122.
     """
     if len(args.fix) == 0:
         return parser.print_help()
-    if not set(args.fix).issubset(string.ascii_uppercase + string.ascii_lowercase):
+
+    letter_set = string.ascii_uppercase + string.ascii_lowercase
+    if not set(args.fix).issubset(letter_set):
         return parser.print_help()
 
     if args.p and not args.s:
@@ -87,8 +89,9 @@ if __name__ == "__main__":
     import sys
     import string
 
-    ## parsing
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+    # parsing
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
         description='''
         This is a prefix/suffix search program.
         Only one option at the time should be supported:
@@ -97,14 +100,16 @@ if __name__ == "__main__":
     parser.add_argument('fix', type=str, help='''
         prefix or suffix:
         It must contain at least one character.
-        It can only include letters from English ASCII character set in the range from DEC 65 to DEC 122.
+        It can only include letters from English ASCII character set
+        in the range from DEC 65 to DEC 122.
         ''')
-    parser.add_argument('-p', '--prefix', action='store_true', dest='p', default=False, help='prefix search')
-    parser.add_argument('-s', '--suffix', action='store_true', dest='s', default=False, help='suffix search')
+    parser.add_argument('-p', '--prefix', action='store_true',
+                        dest='p', default=False, help='prefix search')
+    parser.add_argument('-s', '--suffix', action='store_true',
+                        dest='s', default=False, help='suffix search')
     args = parser.parse_args(sys.argv[1:])
     args = parser.parse_args()
 
     words = [w.rstrip() for w in sys.stdin.readlines()]
 
     print(control(args, words))
-
